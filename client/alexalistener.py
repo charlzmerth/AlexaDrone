b4db74bf0eca3348f405a6864f82adae5282e5d6
Charlie Merth <charliemerth@gmail.com>
2020-06-03 17:56:54 -0700

    added voice support

--- client/alexalistener.py
+++ client/alexalistener.py
@@ -1,13 +1,29 @@
 import os
+import socket
 from flask import Flask
+from flask import request
 from flask import jsonify
+from urllib.parse import unquote
+import logging
+
+# Turn off logging to stdout
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.ERROR)
+
+# Record local IP address
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(("8.8.8.8", 80))
+url = s.getsockname()[0] + ':5000/static/control.html'
+s.close()
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    print()
     return 'Hello, World!'
 
+# Reveive key presses
 @app.route('/start/<dir>')
 def start(dir):
     if dir == 'forward':
@@ -21,8 +37,10 @@ def start(dir):
     else:
         message = 'invalid direction'
 
+    print('start ' + dir)
     return jsonify({ 'response': message })
 
+# Receive key releases
 @app.route('/stop/<dir>')
 def stop(dir):
     if dir == 'forward':
@@ -36,10 +54,22 @@ def stop(dir):
     else:
         message = 'invalid direction'
 
+    print('stop ' + dir)
     return jsonify({ 'response': message })
 
+# Receive text commands
 @app.route('/command/<string>')
 def command(string):
+    string = unquote(string)
+    print(string)
     return jsonify({ "command" : string })
 
-app.run(host= '0.0.0.0', threaded=True)
+print()
+print('----------------------------------------------')
+print('Starting AlexaDrone Server')
+print('Enter this url in a web browser:')
+print(url)
+print('----------------------------------------------')
+print()
+
+app.run(host= '0.0.0.0', port=5000)
